d099b212c78d98419e7180f4080263b565347b4d
Grzegorz Kotfis <gkotfis@outlook.com>
2020-01-03 21:42:09 +0100

    init RealPong game

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import * as Phaser from 'phaser'
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+    title: 'Real Pong',
+    type: Phaser.AUTO,
+    scale: {
+        width: window.innerWidth,
+        height: window.innerHeight
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    parent: 'game',
+    backgroundColor: '#000000'
+}
+
+export const game = new Phaser.Game(gameConfig);
+
+window.addEventListener('resize', () => {
+    game.scale.refresh();
+})
