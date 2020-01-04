f26aaffa5914e246402523ff386f9976def5f917
Grzegorz Kotfis <gkotfis@outlook.com>
2020-01-04 11:08:15 +0100

    add keyboard controls

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -19,6 +19,7 @@ export class GameScene extends Phaser.Scene {
     private ball: Ball;
     private player_one: Paddle;
     private player_two: Paddle;
+    private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
 
     public create() {
 
@@ -28,9 +29,20 @@ export class GameScene extends Phaser.Scene {
         this.ball = new Ball(this, this.game.scale.width / 2, this.game.scale.height / 2)
         this.player_one = new Paddle(this, 40, this.game.scale.height / 2, 20, 80, 0xFFFFFF);
         this.player_two = new Paddle(this, this.game.scale.width - 40, this.game.scale.height / 2, 20, 80, 0xFFFFFF);
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     public update() {
+
+        if (this.cursors.down.isDown) {
+            this.player_one.y += 8;
+            this.player_two.y += 8;
+        }
+        else if (this.cursors.up.isDown) {
+            this.player_one.y -= 8;
+            this.player_two.y -= 8;
+        }
+
         this.physics.world.collide(this.ball, this.topWall, null, null, this);
         this.physics.world.collide(this.ball, this.bottomWall, null, null, this);
         this.physics.world.collide(this.ball, this.player_one);
